eefebbc9917970130a1984563b06f8006721b8a9
Sean <sean.n.cordes@gmail.com>
2018-11-05 12:44:56 -0500

    final?

--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -1,5 +1,4 @@
 require 'mechanize'
-require 'rake'
 
 module SearchService
   class << self
--- lib/tasks/heroku_restart.rb
+++ lib/tasks/heroku_restart.rb
@@ -2,5 +2,5 @@ desc 'heroku_restart'
 task heroku_restart: :environment do
   heroku = PlatformAPI.connect_oauth(ENV['OAUTH_TOKEN'])
   dynos =  heroku.dyno.list(ENV['HEROKU_APP_NAME'])
-  heroku.dyno.restart(ENV['HEROKU_APP_NAME'], dynos[0]['name'])
+  heroku.dyno.restart(ENV['HEROKU_APP_NAME'])
 end
